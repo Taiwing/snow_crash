b34dcb32fa87fc73b798cd15be11fedf4613563b
yoann foreau <yoann.foreau@ito.am>
2022-07-01 17:50:10 +0200

    chore: remove useless files

--- level06/Resources/dup.php
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/php
-<?php
-function y($m) { print "\nWOO@OO\n"; print "\nINPUT_START\n$m\nINPUT_END\n"; $m = preg_replace("/\./", " x ", $m); $m = preg_replace("/@/", " y", $m); print "\nOUTPUT_START\n$m\nOUTPUT_END\n"; return $m; }
-function x($y, $z) { print "\nHOLLA\n"; $a = file_get_contents($y); print "\n1\n"; $a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a); print "\n2\n"; $a = preg_replace("/\[/", "(", $a); print "\n3\n"; $a = preg_replace("/\]/", ")", $a); print "\n4\n"; return $a; }
-$r = x($argv[1], $argv[2]); print $r;
-?>
--- level10/switch.bash
+++
@@ -1,1 +0,0 @@
-ln -sf -T $HOME/token /tmp/lol
